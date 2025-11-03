fe4620a29c42b5d77418768f37759f1735a0ffa9
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-11-03 09:44:40 +0530

    FIRST COMMIT!

---
+++ app/src/main/java/com/example/exmate_sdp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.exmate_sdp;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.exmate_sdp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolBar);
+        getSupportActionBar().setTitle("Transactions");
+        binding.floatingActionButton2.setOnClickListener(c->{
+            new AddTransactionFragment().show(getSupportFragmentManager(),null);
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}

7909221f6d407d03dfaa90719affabd721c287ee
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-11-29 12:49:31 +0530

    Coding DOne

--- app/src/main/java/com/example/exmate_sdp/views/activites/LoginActivity.java
+++ app/src/main/java/com/example/exmate_sdp/views/activites/LoginActivity.java
@@ -1,4 +1,71 @@
 package com.example.exmate_sdp.views.activites;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.exmate_sdp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private Button loginButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_Button);
+
+       loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
+
+                if(!email.isEmpty()&& Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    if(!password.isEmpty())
+                    {
+                        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                            }
+                        }).addOnFailureListener(e -> Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show());
+                    }
+                    else
+                    {
+                        loginPassword.setError("Enter Password");
+                        loginPassword.requestFocus();
+                    }
+                }
+                else
+                {
+                    loginEmail.setError("Enter Email");
+                    loginEmail.requestFocus();
+                    }
+                }
+
+
+            });
+    }
+
+
 }
--- app/src/main/java/com/example/exmate_sdp/views/activites/SignUpActivity.java
+++ app/src/main/java/com/example/exmate_sdp/views/activites/SignUpActivity.java
@@ -1,4 +1,78 @@
 package com.example.exmate_sdp.views.activites;
 
-public class SignUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.exmate_sdp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword, signupName, signupCPassword;
+    private Button signupButton;
+
+
+
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        auth=FirebaseAuth.getInstance();
+        signupEmail=findViewById(R.id.signup_email);
+        signupPassword=findViewById(R.id.signup_password);
+        signupButton=findViewById(R.id.signup_button);
+
+        signupButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String user=signupEmail.getText().toString().trim();
+                        String pass=signupPassword.getText().toString().trim();
+
+                        if(user.isEmpty())
+                        {
+                            signupEmail.setError("Enter Email");
+                            signupEmail.requestFocus();
+                        }
+                        if (pass.isEmpty())
+                        {
+                            signupPassword.setError("Enter Password");
+                            signupPassword.requestFocus();
+                        }
+                       else
+                        {
+                            auth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        Toast.makeText(SignUpActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
+                                        finish();
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(SignUpActivity.this,"User Not Created",Toast.LENGTH_SHORT).show();
+                                    }
+
+
+                                }
+                            });
+
+                        }
+
+                    }
+                }
+        );
+
+    }
 }
